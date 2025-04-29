d38a2977eacd6703bf2312a59105c4864ca32299
Alvin <alvin.chen.718@gmail.com>
2025-04-29 16:55:17 -0400

    Refactor: created socket store to replace useSocket hook, centralize logic

---
+++ src/store/socketStore.ts
@@ -0,0 +1,39 @@
+import { create } from 'zustand';
+import { Socket } from 'socket.io-client';
+import { initializeSocket, disconnectSocket, SocketEvents } from '@/lib/socket';
+import { User } from '@/types';
+
+interface SocketState {
+  socket: Socket | null;
+  isConnected: boolean;
+
+  // Actions
+  connect: (user: User) => void;
+  disconnect: () => void;
+}
+
+export const useSocketStore = create<SocketState>((set) => ({
+  socket: null,
+  isConnected: false,
+
+  connect: (user) => {
+    if (!user) return;
+
+    const socketInstance = initializeSocket(user);
+
+    socketInstance.on(SocketEvents.CONNECT, () => {
+      set({ isConnected: true });
+    });
+
+    socketInstance.on(SocketEvents.DISCONNECT, () => {
+      set({ isConnected: false });
+    });
+
+    set({ socket: socketInstance });
+  },
+
+  disconnect: () => {
+    disconnectSocket();
+    set({ socket: null, isConnected: false });
+  },
+}));
