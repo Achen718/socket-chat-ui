71b7fa42449bcf7c293aba61b66518942975f799
Alvin <alvin.chen.718@gmail.com>
2025-04-30 18:11:53 -0400

    Update main layout with new context

--- src/app/chat/layout.tsx
+++ src/app/chat/layout.tsx
@@ -1,11 +1,14 @@
-'use client';
-
 import { MainLayout } from '@/components/layout/MainLayout';
+import { SideNavProvider } from '@/context/SideNavContext';
 
 export default function ChatLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <MainLayout>{children}</MainLayout>;
+  return (
+    <SideNavProvider>
+      <MainLayout>{children}</MainLayout>
+    </SideNavProvider>
+  );
 }
