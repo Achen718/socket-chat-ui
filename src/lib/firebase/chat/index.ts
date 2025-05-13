3d58c31e8736e2e85bbb21ff9aefce1019f8fb53
Alvin <alvin.chen.718@gmail.com>
2025-05-13 16:52:05 -0400

    refactor chat.ts - migration service

--- src/lib/firebase/chat/index.ts
+++ src/lib/firebase/chat/index.ts
@@ -1,5 +1,5 @@
 // Re-export all services for easy import
 export * from './messageService';
 export * from './conversationService';
-// export * from './realtimeService'; // Will add later
+export * from './realtimeService';
 // export * from './migrationService'; // Will add later
